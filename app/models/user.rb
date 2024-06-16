49fa5b0c36ee4ecc212e07cc42b0c70b09407685
Chris Shao <bjchris32@gmail.com>
2024-06-15 23:54:52 -0500

    feat(user model): define user model and schema with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_secure_password
+
+  validates_uniqueness_of :email
+  validates :password, length: { minimum: 6, maximum: 20 }
 end
