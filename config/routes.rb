c7785034f535d9768657dbfbad3e31bdb53ce825
Chris Shao <bjchris32@gmail.com>
2024-06-16 00:01:26 -0500

    feat(routes, controllers): implement login system controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'home#index'
 end
