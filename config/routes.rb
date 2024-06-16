488ffaa3c80910a5293390bce0db0bd1f873362d
Chris Shao <bjchris32@gmail.com>
2024-06-16 11:59:05 -0500

    feat(routes): use delete action and remove duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
 
   root 'home#index'
 end
