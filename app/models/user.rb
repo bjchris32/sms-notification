56172c0ed9ffc6ec39dd57b101f17f2cd5a1dd09
Chris Shao <bjchris32@gmail.com>
2024-06-17 16:52:36 -0500

    feat: broadcast the partial after user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email
   validates :password, length: { minimum: 6, maximum: 20 }
+
+  after_create_commit -> { broadcast_replace_to "users", partial: "users/user_count_message", locals: { user_count: User.count }, target: "users_count_message" }
 end
