0661e03344a8174c3e5452793906e1c01a2188ab
Chris Shao <bjchris32@gmail.com>
2024-06-17 23:21:21 -0500

    feat(sessions controller): adjust parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by_email(user_params[:email]) || User.new(email: user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Logged in!"
     else
@@ -17,5 +17,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
